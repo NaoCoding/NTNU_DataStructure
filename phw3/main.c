8021af7e89511c0f2b0d5d3458a0d19370bf6978
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-11-30 01:03:05 +0800

    update phw3

--- phw3/main.c
+++ phw3/main.c
@@ -1,7 +1,15 @@
 #include "binary_tree.h"
-#include "ui.h"
+#include <string.h>
 
 int main(){
 
+    while(1){
+        printf("\e[1;35mPlease enter an infix expression and press enter:\e[0m\n");
+        char infix[1005];
+        scanf("%s", infix);
+        if(strlen(infix) > 20 || strlen(infix) < 5){
+            printf("\e[1;31mError : Length of the infix expression should between 5 ~ 20\n");
+        }
+    }
 
 }
